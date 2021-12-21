e10d1770abec3a7baa1ea593272630fbacbf9af5
Auqherus <patrykromanowski@gmail.com>
2021-12-21 22:19:40 +0100

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
@@ -16,6 +14,32 @@ public class Main {
         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
         *  */
+
+        int a = 1;
+        int b = 3;
+        double a1 = 2.5;
+        double b1 = 10.745;
+        short c = 3;
+        short c1 = 673;
+        long d = 6784563;
+        long d1 = 1098293049;
+        byte e = 3;
+        byte e1 = 33;
+        String name = "Tomek";
+        char key = '2';
+
+        System.out.println("Show a:"+" "+a);
+        System.out.println("Show b:"+" "+b);
+        System.out.println("Show a1:"+" "+a1);
+        System.out.println("Show b1:"+" "+b1);
+        System.out.println("Show c:"+" "+c);
+        System.out.println("Show c1:"+" "+c1);
+        System.out.println("Show d:"+" "+d);
+        System.out.println("Show d1:"+" "+d1);
+        System.out.println("Show e:"+" "+e);
+        System.out.println("Show e1:"+" "+e1);
+        System.out.println("Show Name:"+" "+name);
+        System.out.println("Show Number:"+" "+key);
 
 
     }
