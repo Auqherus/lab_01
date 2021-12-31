9f778b618c9b37094e4b67033fc0297587ff839b
Auqherus <patrykromanowski@gmail.com>
2021-12-31 14:41:49 +0100

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.math.*;
 
 public class Main {
 
@@ -9,11 +11,11 @@ public class Main {
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
 
         /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
 
       /*  int a = 1;
         int b = 3;
@@ -42,16 +44,96 @@ public class Main {
         System.out.println("Show Number:"+" "+key);
 */
 
-        double a1,b1;
-
         Scanner scan = new Scanner(System.in);
-        System.out.print("Podaj a :"+" ");
-        int a = scan.nextInt();
-        System.out.print("Podaj b :"+" ");
-        int b = scan.nextInt();
+
+        int dodaj = 1;
+        int minus = 2;
+        int mnozenie = 3;
+        int dzielenie = 4;
+        int modulo = 5;
 
 
-        System.out.println("Wynik to ="+" "+a*b);
+        System.out.println("Wpisz 'q' by zakonczyc lub enter, by kontynuowac");
+        String end = scan.toString();
+        while  (end != "q")  {
+        System.out.println();
+        System.out.println("Wybierz 'q' by zakonczyc!");
+        System.out.println();
+        System.out.println("Wybierz działanie :");
+        System.out.println("1. Dodawanie");
+        System.out.println("2. Odejmowanie");
+        System.out.println("3. Mnożenie");
+        System.out.println("4. Dzielenie");
+        System.out.println("5. Modulo");
+        int c = scan.nextInt();
 
+
+        if (c == dodaj) {
+            System.out.println("Wybrales dodawanie!");
+            System.out.print("Podaj a :" + " ");
+            long a = scan.nextLong();
+            System.out.print("Podaj b :" + " ");
+            long b = scan.nextLong();
+
+
+            System.out.println("Wynik dodawania to =" + " " + (a+b));
+
+        } else if (c == minus) {
+            System.out.println("Wybrales odejmowanie!");
+            System.out.print("Podaj a :" + " ");
+            long a = scan.nextLong();
+            System.out.print("Podaj b :" + " ");
+            long b = scan.nextLong();
+
+
+            System.out.println("Wynik odejmowania to =" + " " + (a-b));
+        }
+        else if (c == mnozenie) {
+            System.out.println("Wybrales mnozenie!");
+            System.out.print("Podaj a :" + " ");
+            double a = scan.nextInt();
+            System.out.print("Podaj b :" + " ");
+            double b = scan.nextInt();
+
+
+            System.out.println("Wynik mnozenia to =" + " " + (a*b));
+        }
+        else if (c == dzielenie) {
+            System.out.println("Wybrales dzielenie!");
+            System.out.print("Podaj a :" + " ");
+            double a = scan.nextDouble();
+            System.out.print("Podaj b :" + " ");
+            double b = scan.nextDouble();
+            if(b == 0){
+                System.out.println("Nie mozna dzielic przez 0!");
+            }
+            else
+
+            System.out.println("Wynik dzielenia to =" + " " + (a/b));
+        }
+            if (c == modulo) {
+                System.out.println("Wybrales dzialanie Modulo!");
+                System.out.print("Podaj a :" + " ");
+                double a = scan.nextDouble();
+                System.out.print("Podaj b :" + " ");
+                double b = scan.nextDouble();
+
+
+                System.out.println("Wynik dzialania modulo to =" + " " + (a%b));
+            }
+        else if (end == "q") {
+            System.out.println("Koniec Programu!");
+        }
+        else
+        {
+            System.out.println("Numer spoza zakresu!");
+        }
+
+
+        }
+
+
+
+        }
     }
-}
+
