d03d29a7b33b47823c83c6acbd6ceab11452aa38
Auqherus <patrykromanowski@gmail.com>
2022-01-03 19:15:19 +0100

    ćw 5

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,47 @@ public class Main {
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
+        Scanner sc = new Scanner(System.in);
+
+
+
+   System.out.print("Podaj wiek :" +" ");
+        double age = sc.nextInt();
+
+
+
+       /*
+       //zad 1//
+        int calc = age%3;
+        if(calc == 0){
+            System.out.println("Wiek podzielny przez 3!");
+        }
+        else {
+            System.out.println("Wiek niepodzielny przez 3!");
+        }
+        */
+
+       /* //zad 2//
+        String calc;
+
+        System.out.println(calc = age%2 == 0 ? "Numer indeksu jest parzysty!" : "Numer indeksu nie jest parzysty!");
+        */
+
+        //zad 3//
+
+        if (age >=0 && age%2 == 0){
+            System.out.println("Liczba :"+" "+age+" "+ "jest liczbą dodatnią i parzysta!");
+        }
+        else if(age <=0 && age%2 == 0) {
+            System.out.println("Liczba :"+" "+age+" "+"jest ujemna i parzysta!");
+        }
+        else if(age >=0 && age%2 != 0) {
+            System.out.println("Liczba :"+" "+age+" "+"jest liczbą dodatnią i nieparzystą!");
+        }
+        else if(age <=0 && age%2 != 0) {
+            System.out.println("Liczba :"+" "+age+" "+"jest ujemna i nieparzysta!");
+        }
+
 
     }
 }
